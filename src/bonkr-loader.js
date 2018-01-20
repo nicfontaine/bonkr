b6d2c4f66d4f376b5ae9fd54ef2573a80803f964
Nic Fontaine <ngpfontaine@protonmail.com>
2018-01-19 16:50:38 -0800

    + loader, & disabled styles

---
+++ src/bonkr-loader.js
@@ -0,0 +1,14 @@
+const loader = {
+  cont: document.getElementById('loader'),
+  offset: 500,
+  kill: function() {
+    setTimeout(function() {
+      loader.cont.classList.remove('show')
+      window.removeEventListener('load', loader.kill)
+    },loader.offset)
+  }
+}
+
+if (loader.cont !== null) {
+  window.addEventListener('load', loader.kill)
+}
